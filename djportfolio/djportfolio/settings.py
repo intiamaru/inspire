fe3fb15c1227a690a2af21fb9d87f8c13c28807e
Jorge Boneu <jorgeboneu@gmail.com>
2021-08-10 20:42:27 -0500

    deploy

--- djportfolio/djportfolio/settings.py
+++ djportfolio/djportfolio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'pages',
     'portfolio',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,3 +131,10 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Heroku
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500) 
+# DATABASES['default'].update(db_from_env)
+
+
--- djportfolio/pages/tests.py
+++ djportfolio/pages/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from .views import home_view
+
+class homeTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        #response = self.client.get('/')
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        #response = self.client.get(reverse('home'))
+        self.assertEqual(self.response.status_code,200)
+
+    def test_homepage_template(self):
+        #response = self.client.get('/')
+        self.assertTemplateUsed(self.response, 'pages/home.html')
+
+    def test_homepage_contains_correct_html(self):
+        #response = self.client.get('/')
+        self.assertContains(self.response, 'About')
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        #response = self.client.get('/')
+        self.assertNotContains(self.response, 'This code should not be on the page')
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            home_view.__name__
+        )
